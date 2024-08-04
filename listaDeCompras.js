78a0ed9a30b61877932fbf27cd221f9275ed4414
Kauê Alves Silva <126356237+Kaue-Alves@users.noreply.github.com>
2024-08-04 00:31:37 -0300

    menu feito
    Co-authored-by: ArturMSilva <ArturMSilva@users.noreply.github.com>

--- listaDeCompras.js
+++ listaDeCompras.js
@@ -1,4 +1,8 @@
 import { adicionaItem } from "./functions/adicionaItem";
+import { listarItens } from "./functions/listarItens";
+import { editarItem } from "./functions/editarItem";
+import { removeItem } from "./functions/removeItem";
+import { situation } from "./functions/situation";
 
 const menu = `
 MENU - LISTA DE COMPRAS
@@ -8,7 +12,49 @@ MENU - LISTA DE COMPRAS
     4 - REMOVER ITEM
     5 - MARCAR COMO COMPRADO OU NÃO COMPRADO
     6 - RESUMO DA LISTA
+    0 - SAIR
 `
-let listaDeCompra = []
-adicionaItem(listaDeCompra)
-console.log(listaDeCompra);
+let listaDeCompra = [
+    {
+        nome: "Abacaxi",
+        quantidade: 2,
+        categoria: "Fruta",
+        status: "comprado ✅"
+    },{
+        nome: "Prego",
+        quantidade: 50,
+        categoria: "Ferramenta",
+        status: "não comprado ❌"
+    }
+]
+
+let i = true;
+while(i) {
+    console.log(menu);
+    let escolha = Number(prompt("Escolha uma opção:"));
+    switch(escolha) {
+        case 1:
+            adicionaItem(listaDeCompra)
+            break;
+        case 2:
+            listarItens(listaDeCompra)
+            break;
+        case 3:
+            editarItem(listaDeCompra)
+            break;
+        case 4:
+            removeItem(listaDeCompra)
+            break;
+        case 5:
+            situation(listaDeCompra)
+            break;
+        case 6:
+            break;
+        case 0:
+            console.log("Obrigado pela preferência! Até logo!");
+            i = false;
+            break;
+        default:
+            console.log("Opção Inválida!! Escolha uma opção existente.");
+    }
+}
