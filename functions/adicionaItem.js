b8a79a6590cf882fc8f562855ec16a32ca970cc1
Kauê Alves Silva <126356237+Kaue-Alves@users.noreply.github.com>
2024-08-04 00:32:40 -0300

    função adicionar item

--- functions/adicionaItem.js
+++ functions/adicionaItem.js
@@ -1,15 +1,18 @@
+import { validar } from "./validar"
+
 export const adicionaItem = (lista) => {
     const nomeItem = prompt("Nome do Item:")
     const quantidade = Number(prompt("Quantidade:"))
     const categoria = prompt("Categoria:")
 
-    if(nomeItem === null || quantidade <= 0 || isNaN(quantidade) || quantidade === null || categoria === null){
+    if (validar(nomeItem, quantidade, categoria) == false) {
         console.log("Algum campo está vazio ou inválido, tente novamente preenchendo todos os campos.");
     } else {
         const obj = {
             nome: nomeItem,
             quantidade: quantidade,
-            categoria: categoria
+            categoria: categoria,
+            status: "não comprado ❌"
         }
         lista.push(obj)
         console.log("Item adicionado com sucesso!");
